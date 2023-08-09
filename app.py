77bf7560fbfba32a46945a59c8959ae09d7896ef
EvoltPratom <bishantad@gmail.com>
2023-08-09 16:57:12 +0545

    hellocommit

---
+++ app.py
@@ -0,0 +1,48 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+from PIL import Image
+
+app = Flask(__name__)
+
+# Configure upload folder
+UPLOAD_FOLDER = os.path.join('static', 'uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+
+        # ai jadu here
+        image = Image.open(filepath)
+        processed_filepath = os.path.join(app.config['UPLOAD_FOLDER'], 'processed_' + filename)
+        image.save(processed_filepath)
+
+        return render_template('result.html', original_image=filename, processed_image='processed_' + filename)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
